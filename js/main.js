3e59bb72c4437e493cd05ca223709e4fed96ccd0
juratdev <juratdev@gmail.com>
2023-11-25 13:09:20 +0500

    edited header and footer

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// const emailInput = document.querySelector("#email");
+// const emailBtn = document.querySelector(".btn-email");
+// const err = document.querySelector(".err")
+// emailBtn.addEventListener('click', function (event) {
+//   event.preventDefault();
+
+//   if (emailInput.value === "" || emailInput.value == null) {
+//     err.innerHTML = `Error`
+//   }
+// })
